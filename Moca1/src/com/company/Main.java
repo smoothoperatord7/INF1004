ff3ab6e9e99ac0bab6fa49de166455bcbea4e4d7
Eric Mercier <eric.mercier1500@gmail.com>
2020-05-21 00:19:00 -0400

    4ieme commit

--- Moca1/src/com/company/Main.java
+++ Moca1/src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-     System.out.println ("Alexa is hot");
+     System.out.println ("KatVerv");
     }
 }
